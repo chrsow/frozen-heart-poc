cd0c9f054ddd8711e78cc38e9b6f011731690d6b
chrsow <liang.wasin@gmail.com>
2023-02-27 13:30:55 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ fi
 
 if [ ! -e ./circuits/powersOfTau28_hez_final_15.ptau ]; then
     echo "#### Download Powers of Tau ####"
-    wget -q ttps://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau -P circuits
+    wget -q https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau -P circuits
 fi
 
 echo "#### Compile circuit ####"
